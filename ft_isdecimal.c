0bab8e0a0e13d068a342c62311e8ddea8a3fd494
Thomas <tchalifour91@gmail.com>
2022-10-18 10:09:37 -0400

    Adding ft_strisdecimal.c

---
+++ ft_isdecimal.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdecimal.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchalifo <tchalifo@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 09:37:10 by tchalifo          #+#    #+#             */
+/*   Updated: 2022/10/18 10:06:53 by tchalifo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strisdecimal(char *str)
+{
+	int	i;
+	int bool;
+
+	i = 0;
+	bool = 0;
+	while (str[i])
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			i++;
+		else if (str[i] == '.' && str[i + 1] >= '0' && str[i + 1] <= '9')
+			i++;
+		else
+		{
+			bool = 1;
+			break;
+		}
+	}
+	return (bool);
+}
