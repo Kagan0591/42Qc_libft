ad8e5133838062dd33745f34511da00936713dcb
Thomas <tchalifour91@gmail.com>
2022-10-25 12:10:10 -0400

    Have fix the ft_argvcpy duplication probleme

--- ft_argvcpy.c
+++ ft_argvcpy.c
@@ -6,7 +6,7 @@
 /*   By: tchalifo <tchalifo@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 16:44:06 by tchalifo          #+#    #+#             */
-/*   Updated: 2022/10/19 09:03:46 by tchalifo         ###   ########.fr       */
+/*   Updated: 2022/10/25 12:07:55 by tchalifo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@ char	**ft_argvcpy(int argc, char **argv)
 	int		i;
 
 	i = 0;
-	argv2 = malloc(sizeof(char *) * argc);
-	while (argc >= 0)
+	argv2 = ft_calloc(argc, sizeof(char *));
+	if (!argv2)
+		return (0);
+	while (argv[i])
 	{
-		argv2[i] = argv[i];
-		argc--;
+		argv2[i] = ft_strdup(argv[i]);
 		i++;
 	}
 	return (argv2);
