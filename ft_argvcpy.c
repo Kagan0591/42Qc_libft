25b3786810bb8eeb49fbb2a300dea8bb8951c60c
Thomas <tchalifour91@gmail.com>
2022-10-25 16:47:44 -0400

    ft_argvcpy fix

--- ft_argvcpy.c
+++ ft_argvcpy.c
@@ -6,11 +6,12 @@
 /*   By: tchalifo <tchalifo@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 16:44:06 by tchalifo          #+#    #+#             */
-/*   Updated: 2022/10/25 16:27:41 by tchalifo         ###   ########.fr       */
+/*   Updated: 2022/10/25 16:46:42 by tchalifo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	**ft_argvcpy(int argc, char **argv)
 {
@@ -18,13 +19,25 @@ char	**ft_argvcpy(int argc, char **argv)
 	int		i;
 
 	i = 0;
-	argv2 = ft_calloc(argc + 1, sizeof(char *));
-	if (argv2 != NULL)
+	i = 0;
+	argv2 = malloc((argc + 1) * sizeof(char *));
+	if (argv2 == NULL)
 		return (NULL);
 	while (argv[i] != NULL)
 	{
 		argv2[i] = ft_strdup(argv[i]);
 		i++;
 	}
+	argv[i] = NULL;
 	return (argv2);
+}
+
+int	main(int argc, char **argv)
+{
+	char **test;
+
+	printf("bob\n");
+	test = ft_argvcpy(argc, argv);
+	printf("%s\n", test[0]);
+	return(0);
 }
